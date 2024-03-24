88e8eff677ff014514b4d26cba62a17c1e4a1060
Julia Łuszkiewicz <julialuszkiewiczz@gmail.com>
2024-03-24 12:06:04 +0100

    wersja nr2

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
-bok_a = 7.1
-pole_kw = (bok_a)^2
-print pole_kw
+def oblicz_pole_trojkata(podstawa, wysokosc):
+    pole = 0.5 * podstawa * wysokosc
+    return pole
 
+try:
+    podstawa = float(input("Podaj długość podstawy trójkąta: "))
+    wysokosc = float(input("Podaj wysokość trójkąta: "))
+    
+    pole = oblicz_pole_trojkata(podstawa, wysokosc)
+    print("Pole trójkąta wynosi:", pole)
+    
+except ValueError:
+    print("Podano nieprawidłową wartość. Wprowadź liczbę.")
+
+def oblicz_pole_prostokata(bok_a, bok_b):
+    pole_p = bok_a*bok_b
+    return pole_p
+try:
+    bok_a = float(input("podaj długość boku a"))
+    bok_b = float(input("podaj dlugosc boku b"))
+
+    pole_p = oblicz_pole_prostokata(bok_a, bok_b)
+    print("pole prostokąta wynosi:", pole_p)
+
+except ValueError:
+    print("Podano nieprawidłową wartość. Wprowadź liczbę.")
+
+def oblicz_pole_kw(bok_kw):
+    pole_k = bok_kw * bok_kw
+    return pole_k
+
+try:
+    bok_kw = float(input("Podaj długość boku kwadratu: "))
+    bok_kw = float(input("Podaj długość boku kwadratu: "))
+
+    pole_k = oblicz_pole_kw(bok_kw)
+    print("Pole kwadratu wynosi:", pole_k)
+
+except ValueError:
+    print("Podano nieprawidłową wartość. Wprowadź liczbę.")
 
 
 
